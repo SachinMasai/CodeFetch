a0ce32668981934dde3f0f1f03536a724c126b02
amandk5 <adkofficial01@gmail.com>
2022-12-23 14:22:39 +0530

    register and login done

--- saas-app/pages/api/main/users.js
+++ saas-app/pages/api/main/users.js
@@ -46,34 +46,9 @@ async function getUsers(req, res) {
 async function addUser(req, res) {
   try {
     await connect();
-    // let { name, email, password, image } = req.body;
-    // const newUser = {
-    //   name: name,
-    //   email: email,
-    //   password: password,
-    //   image: image,
-    // };
-    // console.log(newUser);
-    let userCreds = req.body;
-    const newUser = {
-      name: userCreds.userData.name,
-      email: userCreds.userData.email,
-      password: userCreds.userData.password,
-      image: userCreds.userData.image,
-    };
-    console.log(newUser);
-    // console.log(userCreds);
-    const user = await User.create(userCreds);
+
+    const user = await User.create(req.body);
     res.json({ user });
-    // console.log(req.body);
-    // await User.create(req.body);
-    // let { db } = await connectToDatabase();
-    // await db.collection("users").insertOne(JSON.parse(req.body));
-    // return res.json({
-    //   message: "user created successfully",
-    //   success: true,
-    //   user: user,
-    // });
   } catch (error) {
     return res.json({
       message: new Error(error).message,
--- saas-app/pages/user/login.js
+++ saas-app/pages/user/login.js
@@ -1,15 +1,41 @@
 import { Container, Heading, Input, Button, Box, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "../../redux/Auth/authSlice";
 import { setCookie, getCookie } from "cookies-next";
+import { loginUser } from "../../redux/auth/authSlice";
 
 export default function login() {
   const { token } = useSelector((state) => state.auth);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const dispatch = useDispatch();
   const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+    await fetch("/api/main/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then(() => dispatch(loginUser()))
+      .catch((err) => console.log(err));
+
+    setEmail("");
+    setPassword("");
+  };
 
   useEffect(() => {
     if (token !== null) {
@@ -25,13 +51,28 @@ export default function login() {
   }
 
   return (
-    <Container centerContent>
+    <Container centerContent width="100%" height="100vh" pt="2">
       <Heading>login page</Heading>
       <br />
-      <Input type="email" placeholder="enter email" />
-      <Input type="password" placeholder="enter password" />
-      <br />
-      <Button onClick={() => dispatch(loginUser())}>Login</Button>
+      <form onSubmit={handleLogin}>
+        <Input
+          required
+          value={email}
+          type="email"
+          placeholder="enter email"
+          onChange={({ target }) => setEmail(target.value)}
+        />
+        <Input
+          required
+          value={password}
+          type="password"
+          placeholder="enter password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <br />
+        <Button type="submit">Login</Button>
+        {/* <Button onClick={() => dispatch(loginUser())}>Login</Button> */}
+      </form>
       <br />
       <Box>
         Not Registered? &nbsp;
